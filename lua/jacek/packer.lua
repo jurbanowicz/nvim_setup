ef8592634d852e0e8b8bd4658433c10e389e6694
Jacek Urbanowicz <jurbanowicz202@gmail.com>
2023-12-19 00:42:30 +0100

    added cmp-path

--- lua/jacek/packer.lua
+++ lua/jacek/packer.lua
@@ -33,6 +33,7 @@ return require('packer').startup(function(use)
 			{'neovim/nvim-lspconfig'},
 			-- Autocompletion
 			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-path'},
 			{'hrsh7th/cmp-nvim-lsp'},
 			{'L3MON4D3/LuaSnip'},
 		}
