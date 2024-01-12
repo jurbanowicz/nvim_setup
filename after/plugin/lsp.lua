c324651f943169b62bd5e5ce7ecb4a4dd10739f9
Jacek Urbanowicz <jurbanowicz202@gmail.com>
2024-01-12 13:36:07 +0100

    ADD: enter to confirm autocomplete

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -64,6 +64,7 @@ cmp.setup({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
     ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
     preselect = 'item',
