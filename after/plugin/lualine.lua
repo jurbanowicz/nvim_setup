6e8dd684ef2f35d6e809324203e443364b919819
jurbanowicz <jurbanowicz@student.agh.edu.pl>
2024-05-09 10:45:59 +0200

    change lualine to siplay relative path

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -17,10 +17,11 @@ require('lualine').setup {
       winbar = 1000,
     }
   },
+
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_c = {{'filename', path = 1}},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
